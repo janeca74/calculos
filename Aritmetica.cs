1f304375091987a9ebe18250f071da599971e9c7
janeca74 <181203897+janeca74@users.noreply.github.com>
2024-09-23 19:17:19 +0100

    Adicionar comentários XML aos métodos

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -11,10 +11,18 @@ namespace Calculos
     /// </summary>
     internal class Aritmetica
     {
+        /// <summary>
+        /// Operação soma
+        /// </summary>
+        /// <returns>Retorna a soma de 2 numeros</returns>
         public int Somar(int x, int y)
         {
             return x * y; 
         }
+        /// <summary>
+        /// Operação subtração
+        /// </summary>
+        /// <returns>Retorna a subtração de 2 numeros</returns>
         public int Subtrair(int x, int y)
         {
             return x - y;
