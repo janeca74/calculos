a17707f026b1fa90eb87cfab20532c8b8fe733cd
janeca74 <181203897+janeca74@users.noreply.github.com>
2024-09-23 19:13:04 +0100

    Adicionar método subtrair

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -15,6 +15,9 @@ namespace Calculos
         {
             return x * y; 
         }
-           
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
     }
 }
